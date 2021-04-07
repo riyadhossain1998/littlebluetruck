9aadfe1ecd40a6c18377e0db392a32bebfe5e819
riyadhossain1998 <riyad.hossain114@gmail.com>
2021-04-07 14:54:53 -0600

    updated

--- public/scripts/story.js
+++ public/scripts/story.js
@@ -75,7 +75,9 @@ var storylines = [
 ];
 
 var startStoryButton = document.getElementById("startStory");
-var continueStoryButton = document.getElementById("continue");
+var continueStoryButton = $("continue");
+
+
 
 var line1 = document.getElementById("line1");
 var line2 = document.getElementById("line2");
@@ -83,36 +85,8 @@ var line3 = document.getElementById("line3");
 var line4 = document.getElementById("line4");
 var count = 0;
 
-var dst1 = 'ws://' + location.host + '/client1';
-var dst2 = 'ws://' + location.host + '/client2';
 
-ws1 = new WebSocket(dst1);
-ws2 = new WebSocket(dst2);
 
-startStoryButton.on("click", () => {
-    startStoryButton.style.display = "none";
-    
-    ws1.onopen = function(event) {
-        ws.send(storylines[0])
-    }
-
-    ws2.onopen = function(event) {
-        ws.send(storylines[0])
-    }
-
-    ws1.onmessage = function(event) {
-        var msg = event.data;
-        line1.innerHTML += msg;
-        
-        
-    }
-    ws2.onmessage = function(event) {
-        var msg = event.data;
-        line1.innerHTML += msg;
-        
-        
-    }
-});
 
 function sendData(client) {
     var msg = {
